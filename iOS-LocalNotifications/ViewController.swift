ea0be1faf22d839e9c8202c2243f076876ce32ba
Naveen Naidu <mnaveennaidu9927@gmail.com>
2017-06-28 13:35:27 +0530

    Added image attachments and Notification Actions.

--- iOS-LocalNotifications/ViewController.swift
+++ iOS-LocalNotifications/ViewController.swift
@@ -7,19 +7,66 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound], completionHandler: {(granted, error) in
+            if granted {
+                print("Notification access granted")
+            } else{
+                print(error?.localizedDescription as Any)
+            }
+        }
+    
+    )
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func notifyBtnTapped(sender: UIButton){
+        scheduleNotification(inSeconds: 2, completion: {success in
+            if success{
+                print("Succesfully schedule")
+            } else {
+                print("Error")
+            }
+        })
     }
+    
+    func scheduleNotification(inSeconds: TimeInterval, completion: @escaping (_ Success: Bool) -> ()){
+        
+        let myImage = "giphy-downsized"
+        guard let imageUrl = Bundle.main.url(forResource: myImage, withExtension: "gif") else {
+            completion(false)
+            return
+        }
+        var attachment: UNNotificationAttachment
+        
+        attachment = try! UNNotificationAttachment(identifier: "myNotification", url: imageUrl, options: .none)
+        
+        let notif = UNMutableNotificationContent()
+        
+        // ONLY FOR EXTENSION
+        notif.categoryIdentifier = "myNotificationCategory"
+        
+        
+        notif.title = "New Notification"
+        notif.subtitle = "These are great!"
+        notif.body = "The new notification"
 
-
+        notif.attachments = [attachment]
+        let notifTrigger = UNTimeIntervalNotificationTrigger(timeInterval: inSeconds, repeats: false)
+        let request = UNNotificationRequest(identifier: "myNotification", content: notif, trigger: notifTrigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: {error in
+            if error != nil{
+                print(error as Any)
+                completion(false)
+            } else{
+                completion(true)
+            }
+        })
+    }
 }
 
